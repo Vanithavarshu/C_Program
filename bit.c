ee33e32e575382a4ccc082dba0d161ba0f7ce29c
Vanithavarshu <dhivakarangappan@gmail.com>
2024-09-03 14:39:00 +0530

    program

---
+++ bit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	char a=10,b=6;
+	printf("a=%d,b=%d\n",a,b);
+	printf("a&b=%d\n",a&b);
+	printf("a|b=%d\n",a|b);
+	printf("a^b=%d\n",a^b);
+	printf("a<<1=%d\n",a<<1);
+	printf("a>>1=%d\n",a>>1);
+	return 0;
+}
+
